fa401b1fb4c9f359741e602f682b22e90f6d4a5e
shinshimizu <shinshimizu99@gmail.com>
2021-10-26 19:37:54 +0700

    Added Scene Changer Script
    Change scene between exploration scene and world menu scene

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(name);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Application.Quit();
     }
 }
